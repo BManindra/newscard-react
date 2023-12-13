b767bfaf571f280a4a03b74ebd2f66e9d352565c
BManindra <manibakkathatla4972@gmail.com>
2023-12-13 15:00:44 +0530

    Your commit message here

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 const NewsItem=(props)=>{
     let { title, description, imageUrl, newsUrl, author, date, source } = props;
+    let alternateImage ='https://www.quantumbalancing.com/images/News2.gif';
     return (
       <div className='my-3'>
         <div className="card" >
           <div style={{ display: 'flex', justifyContent: "flex", position:'absolute',right: "0" }}>
             <span className='badge rounded-pill bg-danger'>{source}</span>
           </div>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img src={!imageUrl ? alternateImage : imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
@@ -18,6 +19,6 @@ const NewsItem=(props)=>{
         </div>
       </div>
     )
-}
+  }
 
 export default NewsItem
