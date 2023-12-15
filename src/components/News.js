d9ccae32dbde2daf3f8d9381943c41fd892259ac
BManindra <manibakkathatla4972@gmail.com>
2023-12-15 14:24:14 +0530

    Updated

--- src/components/News.js
+++ src/components/News.js
@@ -7,8 +7,8 @@ import PropTypes from 'prop-types'
 
 const News = (props) => {
   const [articles, setArticles] = useState([])
-  const [loading, setLoading] = useState(true)
-  const [page, setPage] = useState(1)
+  // const [loading, setLoading] = useState(true)
+  // const [page, setPage] = useState(1)
   // const [totalResults, setTotalResults] = useState(0)
 
   const capitalizeFirstLetter = (string) => {
@@ -17,15 +17,15 @@ const News = (props) => {
 
   const updateNews = async () => {
     props.setProgress(10);
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=ac542cfba886443f874f8b46e67e7196&page=${page}&pageSize=${props.pageSize}`;
-    setLoading(true)
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=ac542cfba886443f874f8b46e67e7196&page=${props.page}&pageSize=${props.pageSize}`;
+    // setLoading(true)
     let data = await fetch(url);
     props.setProgress(30);
     let parsedData = await data.json()
     props.setProgress(70);
     setArticles(parsedData.articles)
     // setTotalResults(parsedData.totalResults)
-    setLoading(false)
+    // setLoading(false)
     props.setProgress(100);
   }
 
@@ -56,7 +56,7 @@ const News = (props) => {
   return (
     <>
       <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '90px' }}>News Card - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
-      {loading && <Spinner />}
+      {/* {loading && <Spinner />} */}
         {/* <InfiniteScroll
           dataLength={articles.length}
           next={fetchMoreData}
